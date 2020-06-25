6bbdb6c22f531f7f43b2da7d5ec3a575cc3fd085
Jessica Stone <jstone4815@gmail.com>
2020-06-25 14:02:35 -0700

    add conditional for if movie is already in db

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -27,6 +27,14 @@ class MoviesController < ApplicationController
   def create 
     movie = Movie.new(movie_params)
 
+    # if movie is already in db, don't add
+    if Movie.find_by(external_id: movie.external_id)
+      render json: {
+        errors: "This movie is already in the database"
+      }, status: :forbidden
+      return
+    end
+
     if movie.save
       # render json: movie.as_json(only: [:id, :title, :overview, :release_date, :image_url, :external_id]), status: :created
       # return
