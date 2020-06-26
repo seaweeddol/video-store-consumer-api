5983f7d4e16fa0955e194ee595f383ada09bc396
Jessica Stone <jstone4815@gmail.com>
2020-06-26 10:39:38 -0700

    remove inventory validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ApplicationRecord
   has_many :customers, through: :rentals
 
   validates :title, uniqueness: true, presence: true
-  validates :inventory, presence: true
+  # validates :inventory, presence: true
 
   def available_inventory
     self.inventory - Rental.where(movie: self, returned: false).length
